b89459987363f37c7bda20012ba84d594f802f54
luong_nam <namlk@solashi.com>
2024-04-15 18:25:21 +0700

    update

--- client/app/[lang]/home/page.jsx
+++ client/app/[lang]/home/page.jsx
@@ -1,5 +1,14 @@
+function wait(ms) {
+    return new Promise(resolve => {
+        throw new Error()
+      setTimeout(resolve, ms);
+    });
+  }
+  
 
-export default function Home() {
+export default async function Home() {
+    await wait(1000 * 5)
+
     return <section>
         Homepage
     </section>
