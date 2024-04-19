fa82a46f61cbf193c666ab9deece8801827fa7ea
luong_nam <namlk@solashi.com>
2024-04-19 10:58:04 +0700

    update

---
+++ library/app/page.js
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button variant="text">Text</Button>
+    </main>
+  );
+}
